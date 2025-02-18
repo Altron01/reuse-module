949cf01403c2332d5070b59b88c8df67010ec968
Altron01 <diegoramirez194@gmail.com>
2025-02-18 01:19:29 -0500

    Add files via upload

---
+++ .github/scripts/replace_envvar.py
@@ -0,0 +1,36 @@
+import pathlib
+import re
+import os
+#pip install azure-keyvault-secrets azure-identity
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+
+destination_root = r"./factoryData"
+pattern = "**/*.json"
+
+def readFile(fileName):
+  file = open(fileName, "r")
+  txt = file.read()
+  file.close()
+  return txt
+pass
+
+VAULT_URL = os.environ["VAULT_URL"]
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=VAULT_URL, credential=credential)
+
+master_list = list(pathlib.Path(destination_root).glob(pattern))
+for file in master_list:
+  txt = readFile(str(file.absolute()))
+  match_list = re.findall(r'@{([a-z|A-Z|\\(|\\)|\\.]*)}', txt)
+  if len(match_list) == 0:
+    continue
+
+  for match in match_list:
+    txt = txt.replace("@{" + match + "}", client.get_secret(match.split(".")[1]).value)
+
+  f = open(str(file.absolute()), "w")
+  f.seek(0)
+  f.truncate()
+  f.write(txt)
+  f.close()
